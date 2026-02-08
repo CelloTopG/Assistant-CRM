b988c6eefefa903d429feaaf7060c309f4dae01f
QuantumSolver <marcquesmouton1992@gmail.com>
2026-02-08 05:57:01 +0000

    fix(campaign-template): Add missing Template Filter and Template Channel child doctypes
    - Created Template Filter child doctype for default_filters field
      - Fields: field, operator, value
    - Created Template Channel child doctype for default_channels field
      - Fields: channel_type, is_active
    - Added missing default_channels Table field to Campaign Template JSON
      (was in field_order but not defined in fields array)
    
    Fixes 'Template Filter not found' error when opening Campaign Template

---
+++ assistant_crm/doctype/template_channel/template_channel.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025, WCFCB and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class TemplateChannel(Document):
+    """Child table for Campaign Template default channels"""
+    pass
+
---
+++ assistant_crm/doctype/template_filter/template_filter.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025, WCFCB and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class TemplateFilter(Document):
+    """Child table for Campaign Template default filters"""
+    pass
+
